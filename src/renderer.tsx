bd29ddc19a1aa75bca22d0d06d668b466964f142
Evan Louie <evan.louie@microsoft.com>
2018-04-09 11:05:35 -0700

    call things in path

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -5,15 +5,33 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import * as child from "child_process";
-import { promisify } from "util";
+// import { promisify } from "util";
 
 class App extends React.Component {
+  public static async callACS(): Promise<string> {
+    return new Promise<string>(resolve => {
+      child.exec("acs-engine", (error, stdout, stderr) => {
+        error && console.error(error.message);
+        stderr && console.error(stderr);
+        stdout && console.log(stdout);
+        return resolve(stdout);
+      });
+    });
+  }
 
   public render() {
     return (
       <div className="App">
         <h1 className="header">THIS IS REACT</h1>
         <div>Do whatever you want now!</div>
+        <button
+          className="acs-test"
+          onClick={() => {
+            App.callACS();
+          }}
+        >
+          click me!
+        </button>
       </div>
     );
   }

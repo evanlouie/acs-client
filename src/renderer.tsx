166c94216d7fee41a31d1feedaf6af715d5f7ee8
Evan Louie <evan.louie@microsoft.com>
2018-04-09 10:54:01 -0700

    updated tslint

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,11 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import * as child from "child_process";
+import { promisify } from "util";
+
 class App extends React.Component {
+
   public render() {
     return (
       <div className="App">
